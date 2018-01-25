3b8993d77756a21e3de67128367641849785e793
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-25 19:47:03 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,12 +1,9 @@
 require_relative "./vehicle.rb"
 
 class Car < Vehicle
-# class Car
-#   attr_accessor :wheel_size, :number
-#
-#   def initialize(wheel_size, number)
-#     @wheel_size = wheel_size
-#     @number = number
-#   end
-#
-end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+end 
